f66761780c37dba0d06344d32072168122aa4555
An Le <leducany04@gmail.com>
2025-08-04 12:51:06 -0400

    Minor changes due to typos. Read last commit to see the major changes I have made

--- pybmc/bmc.py
+++ pybmc/bmc.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as pltimport 
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 import os
 import sys
